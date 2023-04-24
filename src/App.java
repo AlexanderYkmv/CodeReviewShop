c66d42b0b9881b0fd40f8cc7a1d6a5811b160fa8
AlexanderYkmv <sasho.yakimov@gmail.com>
2023-04-24 21:57:13 +0300

    Shop

--- src/App.java
+++ src/App.java
@@ -2,9 +2,9 @@ public class App {
     public static void main(String[] args) {
         
 
-        SalesConsultant edi = new SalesConsultant();
-        edi.sellProduct(10.0);
-        System.out.println(edi.getMoney());
+        SalesConsultant Peter = new SalesConsultant();
+        Peter.sellProduct(10.0);
+        System.out.println(Peter.getMoney());
 
     }
 }
