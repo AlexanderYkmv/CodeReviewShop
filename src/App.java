140da3f6f9e07f3512f8f86130e2dd864ee1ddd2
AlexanderYkmv <sasho.yakimov@gmail.com>
2023-04-24 18:26:36 +0300

    commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        
+
+        SalesConsultant edi = new SalesConsultant();
+        edi.sellProduct(10.0);
+        System.out.println(edi.getMoney());
+
+    }
+}
